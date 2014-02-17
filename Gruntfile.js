5d8e1c1d35714a33029e54f491b6555e486e8034
yi <yi2004@gmail.com>
2014-02-18 02:13:03 +0800

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,30 +6,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     uglify:{
-       my_target: {
+       "ani-render": {
             files: {
                'dist/sgf-ani-render.min.js': ['lib/BinFileReader.js', 'lib/raphael-min.js', 'lib/sgf-ani-render.js']
                                   }
                          }
-            },
-            //concat: {
-                      //options: {
-                                 //separator: ';'
-                               //},
-    //dist: {
-            //src: ['lib/BinFileReader.js', 'lib/raphael-min.js', 'lib/sgf-ani-render.js'],
-    //dest: 'dist/built.js'
-          //}
-                    //},
-
+            }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task.
-  //grunt.registerTask('default', ['concat', 'uglify']);
   grunt.registerTask('default', ['uglify']);
 
 };
